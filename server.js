731b069af8e7b434ef25fbeea0a972baca02d9c2
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-11-30 22:44:25 -0600

    add helpers.js for created at

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ const sess = {
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 
 
